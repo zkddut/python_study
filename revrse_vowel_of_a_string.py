bcc060019883f3c32c457506d589df4ad97fa60a
zkddut <zkd.dut@gmail.com>
2016-06-28 21:47:01 -0700

    Faster?

--- revrse_vowel_of_a_string.py
+++ revrse_vowel_of_a_string.py
@@ -9,12 +9,16 @@ class Solution(object):
     vowels = []
     s_list = list(s)
     for i in range(0, len(s_list)):
+      count = 0
       if s_list[i] in ['a','e','i','o','u','A','E','I','O','U']:
-        vowels.append(s_list[i])
+        for j in range(count, len(s_list)):
+          if s_list[len(s_list)-j-1] in ['a','e','i','o','u','A','E','I','O','U']:
+            s_list[len(s_list)-j-1],s_list[i] = s_list[i],s_list[len(s_list)-j-1]
+            count = j
+            break;
+      if i >= count:
+        break;
 
-    for i in range(0, len(s_list)):
-      if s_list[i] in ['a','e','i','o','u','A','E','I','O','U']:
-        s_list[i] = vowels.pop()
     str = "".join(s_list)
 
     return str
