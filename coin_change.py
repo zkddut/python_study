44cf0030ed34d273c086f352976139230b0a25fe
zkddut <zkd.dut@gmail.com>
2017-04-08 11:04:25 -0700

    Create coin_change.py

---
+++ coin_change.py
@@ -0,0 +1,27 @@
+You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+Example 1:
+coins = [1, 2, 5], amount = 11
+return 3 (11 = 5 + 5 + 1)
+
+Example 2:
+coins = [2], amount = 3
+return -1.
+
+The code below is perfect. Save for study
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        #python dp solution
+        MAX = float('inf')
+        dp = [0] + [MAX] * amount
+
+        for i in xrange(1, amount + 1):
+            dp[i] = min([dp[i - c] if i - c >= 0 else MAX for c in coins]) + 1
+
+        return [dp[amount], -1][dp[amount] == MAX]
+        
